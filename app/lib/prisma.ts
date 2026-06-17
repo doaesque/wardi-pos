7c0d47f8ffb3c4957ada3a5981e9a572637dcbbc
S.L. Wachyu <slwachyu@gmail.com>
2026-06-17 12:22:53 +0700

    feat: init database schema and prisma config

---
+++ app/lib/prisma.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '../generated/prisma';
+
+// Prevent multiple instances of Prisma Client in development
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
