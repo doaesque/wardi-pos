9bc46d137611e416ef614d12057add407801fd88
S.L. Wachyu <slwachyu@gmail.com>
2026-07-07 19:59:45 +0700

    refactor: clean up prisma configuration

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -3,21 +3,23 @@ import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 const prismaClientSingleton = () => {
-  // setup connection pool
+  // setup connection pool with postgresql
   const pool = new Pool({ connectionString: process.env.DATABASE_URL });
   const adapter = new PrismaPg(pool);
 
-  // inject adapter to client
+  // inject the serverless-compatible adapter to the client
   return new PrismaClient({ adapter });
 };
 
+// declare global variable to hold prisma instance and avoid exhaustion
 declare global {
   var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
 const prisma = globalThis.prisma ?? prismaClientSingleton();
 
-// default export
-export default prisma;
-
+// assign strictly on development environments
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
+
+// default export for ease of access
+export default prisma;
